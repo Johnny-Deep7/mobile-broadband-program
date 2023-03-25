6812bc3d6d703dfa7fa2f8d96a96fe7cbbcecff2
mahongoeng <mahongoeng@cmss.chinamobile.com>
2023-03-25 09:21:40 +0800

    Revert "first"
    This reverts commit 02a8dc186e12e584530cb2fe4a98a3cd3f70ae0f.

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.List;
 
 /**
  * counter控制器
@@ -78,5 +79,5 @@ public class CounterController {
       return ApiResponse.error("参数action错误");
     }
   }
-
+  
 }
