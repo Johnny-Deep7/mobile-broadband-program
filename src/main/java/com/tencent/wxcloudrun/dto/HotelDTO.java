2324b3d08bbc7364296fa0692a842a5a850266f4
yang3666 <2293381927@qq.com>
2023-03-25 10:07:23 +0800

    新建实体类

--- src/main/java/com/tencent/wxcloudrun/dto/HotelDTO.java
+++ src/main/java/com/tencent/wxcloudrun/dto/HotelDTO.java
@@ -1,6 +1,34 @@
 package com.tencent.wxcloudrun.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HotelDTO implements Serializable {
+
+    private String Id;
+    private String Substation;
+    private String CustomerManager;
+    private String ContactWay;
+    private String HotelName;
+    private String IsCovered;
+    private String RoomNumber;
+    private String GroupNumber;
+    private String Address;
+    private String Type;
+    private String Operator;
+    private String EndTime1;
+    private String InternetCharge;
+    private String EndTime2;
+    private String ResponsiblePerson;
+    private String Position;
+    private String PhoneNumber;
+    private String VisitDate;
+    private String VisitInformation;
+    private String DifficultPoint;
 }
--- src/main/java/com/tencent/wxcloudrun/dto/IndustrialParkDTO.java
+++ src/main/java/com/tencent/wxcloudrun/dto/IndustrialParkDTO.java
@@ -1,5 +1,34 @@
 package com.tencent.wxcloudrun.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 //产业园区
-public class IndustrialParkDTO {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IndustrialParkDTO implements Serializable {
+    private String Id;
+    private String Substation;
+    private String CustomerManager;
+    private String ContactWay;
+    private String HotelName;
+    private String IsCovered;
+    private String RoomNumber;
+    private String GroupNumber;
+    private String Address;
+    private String Type;
+    private String Operator;
+    private String EndTime1;
+    private String InternetCharge;
+    private String EndTime2;
+    private String ResponsiblePerson;
+    private String Position;
+    private String PhoneNumber;
+    private String VisitDate;
+    private String VisitInformation;
+    private String DifficultPoint;
 }
