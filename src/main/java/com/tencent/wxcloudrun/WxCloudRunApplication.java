699090cbafd0822af52e7263ff7cfe085ada4537
mahongoeng <mahongoeng@cmss.chinamobile.com>
2023-03-26 19:30:48 +0800

    yml修改成默认的

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = {"com.tencent.wxcloudrun.dao"})
+@MapperScan(basePackages = {"com.tencent.wxcloudrun.mapper"})
 public class WxCloudRunApplication {  
 
   public static void main(String[] args) {
