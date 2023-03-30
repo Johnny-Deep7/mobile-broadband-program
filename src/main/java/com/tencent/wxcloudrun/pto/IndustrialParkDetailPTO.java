a3f19c0b391253e837051e582c626cb584edcda6
mahongoeng <mahongoeng@cmss.chinamobile.com>
2023-03-30 10:52:09 +0800

    前端接收shopId

--- src/main/java/com/tencent/wxcloudrun/pto/IndustrialParkDetailPTO.java
+++ src/main/java/com/tencent/wxcloudrun/pto/IndustrialParkDetailPTO.java
@@ -1,6 +1,7 @@
 package com.tencent.wxcloudrun.pto;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
