1af53f00326f2cbe9a3391ba17138f6fad28154b
mahongoeng <mahongoeng@cmss.chinamobile.com>
2023-03-31 09:48:50 +0800

    登录实体类修改

--- src/main/java/com/tencent/wxcloudrun/pto/LoginPTO.java
+++ src/main/java/com/tencent/wxcloudrun/pto/LoginPTO.java
@@ -1,5 +1,7 @@
 package com.tencent.wxcloudrun.pto;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +11,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@TableName(value = "mbp_log_in")
 public class LoginPTO {
+    @TableField("PHONE_NUMBER")
     String phoneNumber;
+    @TableField("PASSWORD")
     String passWord;
+    @TableField("IS_ADMINISTRATOR")
     @Builder.Default
     Boolean isAdmin = false;
 }
