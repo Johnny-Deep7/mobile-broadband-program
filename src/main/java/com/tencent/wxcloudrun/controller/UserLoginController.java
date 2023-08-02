0cf17da77b2264feb463959ffcde43cc756cfbe1
jiyang_ext <jiyang_ext@cop.cmsoft.com.cn>
2023-08-02 16:42:13 +0800

    新增管理员添加账号接口

--- src/main/java/com/tencent/wxcloudrun/controller/UserLoginController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/UserLoginController.java
@@ -21,6 +21,14 @@ public class UserLoginController {
     @Autowired
     private LoginServiceImpl loginService;
 
+    @PostMapping(value = "/regInitialized")
+    public ApiResponse createLogin(@RequestBody LoginDTO loginDTO) {
+        log.info("开始创建用户登录信息");
+        LoginPTO loginPTO = new LoginPTO();
+        BeanUtils.copyProperties(loginDTO, loginPTO);
+        return loginService.regin(loginPTO);
+    }
+
     //注册用户
     @PostMapping(value = "/regin")
     public ApiResponse regin(@RequestBody LoginPTO loginPTO) {
